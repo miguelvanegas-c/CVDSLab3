46ded39875bbc785c597d6a654844354f0cd0ad7
Miguel Angel Vanegas <miguel.vanegas-c@mail.escuelaing.edu.co>
2025-02-05 07:42:41 -0500

    first commit

---
+++ src/main/java/edu/eci/cvds/tdd/library/App.java
@@ -0,0 +1,10 @@
+package edu.eci.cvds.tdd.library;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
